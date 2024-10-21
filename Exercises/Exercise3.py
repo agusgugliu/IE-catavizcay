46dff58e591a3af43d36349fe221766b6ace2135
Agustín Gugliuzza <agusgugliuzza@gmail.com>
2024-10-21 09:31:26 -0300

    added ExerciseN3

---
+++ Exercises/Exercise3.py
@@ -0,0 +1,24 @@
+'''
+EXERCISE N°3
+Write a program that stores in a dictionary the prices of the fruits in the table, asks the user for a fruit, a number of kilos and displays the price of that number of kilos of fruit. If the fruit is not in the dictionary it should display a message informing about it.
+    Fruit Price:
+    Banana 1.35
+    Apple 0.80
+    Pear 0.85
+    Orange 0.70
+'''
+
+fruit_prices = {
+    'Banana': 1.35,
+    'Apple': 0.80,
+    'Pear': 0.85,
+    'Orange': 0.70
+}
+
+fruit = input('Enter the fruit: ').capitalize()
+
+if fruit in fruit_prices:
+    kilos = float(input('Enter the number of kilos: '))
+    print(f'The price of {kilos} kilos of {fruit} is: {kilos * fruit_prices[fruit]}')
+else:
+    print('The fruit is not in the dictionary')
